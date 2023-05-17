61ba4086e0db5c980268de0010533a6673dec8fb
Yuxi Wang <111711514+wailywang@users.noreply.github.com>
2023-05-17 18:46:52 +0800

    Add files via upload

---
+++ Doodler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doodler : MonoBehaviour
+{
+    public float moveSpeed;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (h*moveSpeed,rb.velocity.y);
+        if (h != 0)
+        {
+            transform.localScale = new Vector3 (-h,1,1);
+        }
+    }
+}
